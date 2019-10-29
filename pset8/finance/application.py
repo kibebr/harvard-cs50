aade4786ed84a406f1fe51981090c90c35f7701c
kibe <47016873+kibebr@users.noreply.github.com>
2019-10-28 20:05:26 -0400

    Perfomance improvements, selling system DONE
    Also added support for AJAX

--- pset8/finance/application.py
+++ pset8/finance/application.py
@@ -7,16 +7,16 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_UP
 import datetime
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required, lookup, usd, usdFloat
 
 # Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
 
 # Ensure responses aren't cached
 @app.after_request
@@ -44,6 +44,17 @@ class DataStore():
     lastQuote = None
 data = DataStore()
 
+class Stock:
+    def __init__(self, symbol, shares):
+        fetchData = lookup(symbol)
+        self.name = fetchData["name"]
+        self.price = fetchData["price"]
+        self.symbol = symbol;
+        self.shares = shares
+    def get_dict(self):
+        return {"symbol": self.symbol, "name": self.name, "price": self.price, "shares": self.shares}
+
+
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
@@ -51,39 +62,28 @@ if not os.environ.get("API_KEY"):
 @app.route("/")
 @login_required
 def index():
-    stocks = {}
-    thisStock = db.execute("SELECT shares, symbol FROM purchases WHERE purchaser = '{}'".format(session["user_id"]))
-
-    for i in range(len(thisStock)):
-        stockSymbol = thisStock[i]["symbol"]
-        if stockSymbol not in stocks.keys():
-            stocks[stockSymbol] = thisStock[i]["shares"]
-        else:
-            stocks[stockSymbol] += thisStock[i]["shares"]
-
-    print("RELOADED!!")
-
-    session["user_stocks"] = stocks
-
-    return render_template("hub.html", stocks=stocks)
+    return render_template("hub.html", stocks=session["user_stocks"])
 
 
-@app.route("/buy", methods=["POST"])
+@app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
-    requestedShares = request.form.get("sharesInput")
-    if not requestedShares:
-        return render_template("hub.html", redirectDiv=0, quoteData=data.lastQuote, error_message="Quantity of shares not specified.")
+    requestedShares = request.args.get("shares")
+    requestedSymbol = request.args.get("sym")
+
+    if not requestedSymbol or not requestedShares:
+        return apology("ERROR")
 
     # standard stock price * how many stocks user requested to purchase
-    totalValue = Decimal(data.lastQuote.get("price").replace("$","")) * int(requestedShares)
+    totalValue = usdFloat(float(data.lastQuote.get("price").replace("$",""))) * int(requestedShares)
     userFinalCash = session["user_cash"] - totalValue
 
     if not userFinalCash >= 0:
-        return render_template("hub.html", redirectDiv=0, quoteData=data.lastQuote, error_message="Could not purchase stocks: not enough cash.")
+        return {"id": "NOT_ENOUGH_CASH"}
     else:
-        purchaseDateTime = datetime.datetime.utcnow()
+        userStocks = session["user_stocks"]
         session["user_cash"] = userFinalCash
+        purchaseDateTime = datetime.datetime.utcnow()
 
         # UPDATE USER'S CASH IN THE DATABASE
         db.execute("UPDATE users SET cash = {} WHERE id = {}".format(userFinalCash, session["user_id"]))
@@ -91,14 +91,18 @@ def buy():
         # INSERTS THE PURCHASE INTO THE 'PURCHASES' TABLE IN THE DATABASE
         db.execute("INSERT INTO purchases ('purchaser', 'price', 'date', 'symbol', 'shares') VALUES ('{}', '{}', '{}', '{}', '{}')".format(session["user_id"], totalValue, purchaseDateTime.strftime('%Y-%m-%d %H:%M:%S'), data.lastQuote.get("symbol"), int(requestedShares)))
 
-        # CHECKS IF USER HAS ALREADY PURCHASED THAT STOCK, IF NOT: CREATE A COLUMN WITH THE STOCK'S SYMBOL AND SHARES. ELSE: JUST UPDATE THE SHARES FOR THAT STOCK
+        # CHECKS IF USER HAS ALREADY PURCHASED THAT STOCK, IF NOT: CREATE A COLUMN WITH THE STOCK'S SYMBOL AND SHARES.
+
         if len(db.execute("SELECT * from total_userShares WHERE userid = '{}' AND symbol = '{}'".format(session["user_id"], data.lastQuote.get("symbol")))) != 1:
             db.execute("INSERT INTO total_userShares ('userid', 'symbol', 'shares') VALUES ('{}', '{}', '{}')".format(session["user_id"], data.lastQuote.get("symbol"), int(requestedShares)))
-        else:
+        else: # JUST UPDATE THE SHARES FOR THAT STOCK
             db.execute("UPDATE total_userShares SET shares = shares + '{}' WHERE userid = '{}' AND symbol = '{}'".format(int(requestedShares), session["user_id"], data.lastQuote.get("symbol")))
 
-        return redirect("/")
-
+        for stock in userStocks: # updates cache
+            if stock["symbol"] == requestedSymbol.upper():
+                stock["shares"] = stock["shares"] + int(requestedShares)
+                flash("Success! Transaction (buying) completed.", category="success")
+                return {"id": "BOUGHT"}
 
     return apology("TODO")
 
@@ -128,11 +132,13 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return render_template("login.html", error_message="Must provide username.")
+            flash("Please type your username.", category="danger");
+            return render_template("login.html")
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return render_template("login.html", error_message="Must provide password.")
+            flash("Please type your password.", category="danger")
+            return render_template("login.html")
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
@@ -141,13 +147,23 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return render_template("login.html", error_message="Invalid username/password.")
+            flash("Username or password are invalid.", category="danger");
+            return render_template("login.html")
 
         # Remember which user has logged in, and store their cash
         session["user_id"] = rows[0]["id"]
-        session["user_cash"] = Decimal(rows[0]["cash"])
+        session["user_cash"] = rows[0]["cash"]
+
+        stocks = db.execute("SELECT symbol, shares FROM total_userShares WHERE userId = :userId", userId=session["user_id"])
+        listOfStocks = []
+
+        for stock in stocks:
+            listOfStocks.append(Stock(stock["symbol"], stock["shares"]).get_dict())
+
+        session["user_stocks"] = listOfStocks
 
         # Redirect user to home page
+        flash("Logged in!", category="success")
         return redirect("/")
 
     # User reached route via GET (as by clicking a link or via redirect)
@@ -159,23 +175,29 @@ def login():
 def logout():
     """Log user out"""
 
-    # Forget any user_id and user_cash
+    # Forget any user_id and user_cash and user_stocks
     session.clear()
 
     # Redirect user to login form
     return redirect("/")
 
-
-@app.route("/quote", methods=["POST"])
+@app.route("/quote", methods=["GET"])
 @login_required
-def quote(): # FIND A WAY TO NOT RELOAD THE PAGE WHEN QUOTING!! and reset the tables plz
-    userInput = request.form.get("symbol")
-    if not userInput:
-        return render_template("hub.html", redirectDiv=0, error_message="Please insert a symbol.")
+def quote():
+    requestedSymbol = request.args.get("sym")
+    isAjax = request.args.get("fromAJAX")
 
-    data.lastQuote = lookup(userInput)
-    print(session["user_stocks"])
-    return render_template("hub.html", redirectDiv=1, quoteData=data.lastQuote)
+    if not requestedSymbol:
+        return "no symbol"
+
+    quoteData = lookup(requestedSymbol)
+    data.lastQuote = quoteData;
+    if quoteData is None:
+        return "NONE", 404
+    else:
+        if not isAjax:
+            return render_template("hub.html", redirectDiv=1, quoteData=quoteData)
+        return quoteData
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -198,9 +220,11 @@ def register():
     if canRegister:
         hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=2)
         db.execute("INSERT INTO 'users' ('username', 'hash') VALUES ('{}', '{}')".format(username, hashed_password))
+        flash("Success! You can now log-in to your new account.", category="success")
         return redirect("/")
     else:
-        return render_template("login.html", error_message="Username already taken.", redirectToRegister=True);
+        flash("Username already taken.", category="danger")
+        return render_template("login.html", redirectToRegister=True);
 
 
 @app.route("/sell", methods=["POST"])
@@ -208,17 +232,26 @@ def register():
 def sell():
     requestedStockToSell = request.args.get("sym")
     requestedSharesToSell = int(request.form.get("shares"))
+    userStocks = session["user_stocks"]
 
-    if requestedStockToSell not in session["user_stocks"]:
-        return render_template("hub.html", error_message="You don't have that stock.")
-    elif requestedSharesToSell > int(session["user_stocks"][requestedStockToSell]):
-        return render_template("hub.html", error_message="You don't have enough shares for that stock.")
-    else:
-        totalValue = Decimal(lookup(requestedStockToSell).get("price").replace("$", "")) * int(requestedSharesToSell)
+    for stock in userStocks:
+        if requestedStockToSell == stock["symbol"]: # user has stock
+            if requestedSharesToSell > int(stock["shares"]): # user doesn't have enough shares
+                flash("You don't have enough shares.", category="danger")
+                return render_template("hub.html")
+            else: # user has enough shares
+                totalValue = usdFloat(float(stock["price"].replace("$", ""))) * int(requestedSharesToSell)
+                db.execute("UPDATE total_userShares SET shares = shares - :sharesToSell WHERE userId = :userId AND symbol = :stockSymbol", sharesToSell=requestedSharesToSell, userId=session["user_id"], stockSymbol=stock["symbol"])
 
+                stock["shares"] = stock["shares"] - requestedSharesToSell
+                session["user_cash"] = session["user_cash"] + totalValue
+                db.execute("UPDATE users SET cash = :cash WHERE id = :userId", cash=session["user_cash"], userId=session["user_id"])
 
+                flash("Success! Transaction (selling) completed.", category="success")
+                return render_template("hub.html")
 
-    return "AH "
+    return {"id":"NO_STOCK_FOUND"}
+
 
 
 def errorhandler(e):
